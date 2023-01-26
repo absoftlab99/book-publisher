fe693287b4e3b29eed420d06bf4d7128afd02c46
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2023-01-27 02:39:37 +0600

    book card completed

--- src/Components/BookCard/BookCard.js
+++ src/Components/BookCard/BookCard.js
@@ -1,9 +1,121 @@
 import React from 'react';
+import book from '../../assets/book.jpg';
+import book2 from '../../assets/book2.png';
+import book3 from '../../assets/book3.png';
+import book4 from '../../assets/book4.jpg';
+import book5 from '../../assets/book5.jpg';
 
 const BookCard = () => {
     return (
-        <div>
-            <h2>This is book card</h2>
+        <div className='mt-5'>
+            <div className='grid grid-cols-10 gap-5 md:gap-8'>
+                {/* book one  */}
+                <div className="col-span-10 md:col-span-2">
+                    <div className="card bg-base-100 shadow-xl image-full hover:bg-none">
+                        <figure className='h-full md:h-96'><img src={book} alt="book" /></figure>
+                        <div className="card-body">
+                            <div className='btn btn-circle border-dashed border-black border-[1px] btn-warning p-1'><i class="fa-solid fa-bolt"></i> 40%</div>
+                            <p></p>
+                            <div className="grid grid-cols-1 place-items-start gap-3">
+                                <div className="tooltip tooltip-right tooltip-accent" data-tip="Add to Cart">
+                                    <button className='btn btn-accent btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-cart-plus"></i></button>
+                                </div>
+                                <div className="tooltip tooltip-right tooltip-primary" data-tip="Buy Now">
+                                    <button className='btn btn-primary btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-dolly"></i></button>
+                                </div>
+                                <div className="tooltip tooltip-right tooltip-secondary" data-tip="Quick View">
+                                    <button className='btn btn-secondary btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-eye"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* book Two  */}
+                <div className="col-span-10 md:col-span-2">
+                    <div className="card bg-base-100 shadow-xl image-full hover:bg-none">
+                        <figure className='h-full md:h-96'><img src={book2} alt="book" /></figure>
+                        <div className="card-body">
+                            <div className='btn btn-circle border-dashed border-black border-[2px] btn-warning p-1'><i class="fa-solid fa-bolt"></i> 40%</div>
+                            <p></p>
+                            <div className="grid grid-cols-1 place-items-start gap-3">
+                                <div className="tooltip tooltip-right tooltip-accent" data-tip="Add to Cart">
+                                    <button className='btn btn-accent btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-cart-plus"></i></button>
+                                </div>
+                                <div className="tooltip tooltip-right tooltip-primary" data-tip="Buy Now">
+                                    <button className='btn btn-primary btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-dolly"></i></button>
+                                </div>
+                                <div className="tooltip tooltip-right tooltip-secondary" data-tip="Quick View">
+                                    <button className='btn btn-secondary btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-eye"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* book Three  */}
+                <div className="col-span-10 md:col-span-2">
+                    <div className="card bg-base-100 shadow-xl image-full hover:bg-none">
+                        <figure className='h-full md:h-96'><img src={book3} alt="book" /></figure>
+                        <div className="card-body">
+                            <div className='btn btn-circle border-dashed border-black border-[2px] btn-warning p-1'><i class="fa-solid fa-bolt"></i> 40%</div>
+                            <p></p>
+                            <div className="grid grid-cols-1 place-items-start gap-3">
+                                <div className="tooltip tooltip-right tooltip-accent" data-tip="Add to Cart">
+                                    <button className='btn btn-accent btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-cart-plus"></i></button>
+                                </div>
+                                <div className="tooltip tooltip-right tooltip-primary" data-tip="Buy Now">
+                                    <button className='btn btn-primary btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-dolly"></i></button>
+                                </div>
+                                <div className="tooltip tooltip-right tooltip-secondary" data-tip="Quick View">
+                                    <button className='btn btn-secondary btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-eye"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* book Four  */}
+                <div className="col-span-10 md:col-span-2">
+                    <div className="card bg-base-100 shadow-xl image-full hover:bg-none">
+                        <figure className='h-full md:h-96'><img src={book4} alt="book" /></figure>
+                        <div className="card-body">
+                            <div className='btn btn-circle border-dashed border-black border-[2px] btn-warning p-1'><i class="fa-solid fa-bolt"></i> 40%</div>
+                            <p></p>
+                            <div className="grid grid-cols-1 place-items-start gap-3">
+                                <div className="tooltip tooltip-right tooltip-accent" data-tip="Add to Cart">
+                                    <button className='btn btn-accent btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-cart-plus"></i></button>
+                                </div>
+                                <div className="tooltip tooltip-right tooltip-primary" data-tip="Buy Now">
+                                    <button className='btn btn-primary btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-dolly"></i></button>
+                                </div>
+                                <div className="tooltip tooltip-right tooltip-secondary" data-tip="Quick View">
+                                    <button className='btn btn-secondary btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-eye"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* book Five  */}
+                <div className="col-span-10 md:col-span-2">
+                    <div className="card bg-base-100 shadow-xl image-full hover:bg-none">
+                        <figure className='h-full md:h-96'><img src={book5} alt="book" /></figure>
+                        <div className="card-body">
+                            <div className='btn btn-circle border-dashed border-black border-[2px] btn-warning p-1'><i class="fa-solid fa-bolt"></i> 40%</div>
+                            <p></p>
+                            <div className="grid grid-cols-1 place-items-start gap-3">
+                                <div className="tooltip tooltip-right tooltip-accent" data-tip="Add to Cart">
+                                    <button className='btn btn-accent btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-cart-plus"></i></button>
+                                </div>
+                                <div className="tooltip tooltip-right tooltip-primary" data-tip="Buy Now">
+                                    <button className='btn btn-primary btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-dolly"></i></button>
+                                </div>
+                                <div className="tooltip tooltip-right tooltip-secondary" data-tip="Quick View">
+                                    <button className='btn btn-secondary btn-circle border-0 bg-opacity-90'><i class="fa-solid fa-eye"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import BookCard from '../../Components/BookCard/BookCard';
+import Slider from '../../Components/Slider/Slider';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
+        <div className='px-6 md:px-12'>
+            <Slider></Slider>
+            <BookCard></BookCard>
         </div>
     );
 };
